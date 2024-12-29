bbbfa7907ec6db103005bf3222a50f5de149e23f
Anamika587 <kumarianamika587@gmail.com>
2024-12-29 15:10:48 +0530

    Initial commit of Employee Management App

---
+++ employee-frontend-app/src/app/update-employee/update-employee.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { FormsModule } from '@angular/forms';
+import { EmployeeService } from '../employee.service';
+import { ActivatedRoute, Router } from '@angular/router';
+@Component({
+  selector: 'app-update-employee',
+  imports: [FormsModule],
+  templateUrl: './update-employee.component.html',
+  styleUrl: './update-employee.component.css',
+})
+export class UpdateEmployeeComponent implements OnInit {
+  employee: Employee = new Employee();
+  id!: number;
+  constructor(
+    private employeeService: EmployeeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+
+    this.employeeService.getEmployeeById(this.id).subscribe(
+      (data) => {
+        this.employee = data;
+      },
+      (error) => console.error()
+    );
+  }
+  onSubmit() {
+    this.employeeService.updateEmployee(this.id, this.employee).subscribe(
+      (data) => {
+        this.goToEmployeeList();
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  goToEmployeeList() {
+    this.router.navigate(['/employees']);
+  }
+}
